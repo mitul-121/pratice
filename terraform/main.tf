adb6bb1d4ca9ec97d17dbf7538f2ab6464fc90c6
Mitul12 <mapipaliya22@gmail.com>
2026-03-12 18:27:15 +0200

    fixed

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,26 @@ resource "aws_subnet" "public" {
   map_public_ip_on_launch = true
 }
 
+resource "aws_subnet" "private1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "eu-central-1a"
+
+  tags = {
+    Name = "private-subnet-1"
+  }
+}
+
+resource "aws_subnet" "private2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "eu-central-1b"
+
+  tags = {
+    Name = "private-subnet-2"
+  }
+}
+
 resource "aws_instance" "app" {
   ami = "ami-0c42fad2ea005202d"
   instance_type = "t3.micro"
@@ -44,6 +64,16 @@ resource "aws_security_group" "ec2_sg" {
   }  
 }
 
+resource "aws_db_subnet_group" "default" {
+  name       = "main-db-subnet-group"
+  subnet_ids = [aws_subnet.private1.id, aws_subnet.private2.id]
+
+  tags = {
+    Name = "Main DB subnet group"
+  }
+}
+
+
 resource "aws_db_instance" "db" {
   allocated_storage = 20
   engine = "mysql"
@@ -52,6 +82,7 @@ resource "aws_db_instance" "db" {
   username = var.db_username
   password = var.db_password
   parameter_group_name = "default.mysql8.0"
+  db_subnet_group_name = aws_db_subnet_group.default.name
   skip_final_snapshot = false
   publicly_accessible = false
 }
