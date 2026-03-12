2854ecd526403f2919f1ada28c4fe77febba4725
Mitul12 <mapipaliya22@gmail.com>
2026-03-12 18:33:02 +0200

    fixed

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ resource "aws_subnet" "public" {
 
 resource "aws_subnet" "private1" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "eu-central-1a"
 
   tags = {
@@ -20,7 +20,7 @@ resource "aws_subnet" "private1" {
 
 resource "aws_subnet" "private2" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
+  cidr_block        = "10.0.3.0/24"
   availability_zone = "eu-central-1b"
 
   tags = {
